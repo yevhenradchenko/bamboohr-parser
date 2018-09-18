23f081b2436c9616287de5974be226286b388bbe
Yevhen Radchenko <37250705+yevhenradchenko@users.noreply.github.com>
2018-09-18 13:47:42 +0300

    unnecessary

--- bamboodirectory.py
+++
@@ -1,8 +0,0 @@
-from PyBambooHR import PyBambooHR
-from pprint import pprint
-
-bamboo = PyBambooHR(subdomain='domain', api_key='apikey')
-
-employees = bamboo.get_employee_directory()
-
-pprint(employees)
