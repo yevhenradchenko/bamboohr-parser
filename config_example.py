0b56af524b90488f9507fb7b05e9367c3b6f91ba
Yevhen Radchenko <yevhen.y.radchenko@gmail.com>
2018-09-19 15:02:20 +0300

    Move endpoints to 'config' files

--- config_example.py
+++ config_example.py
@@ -1,2 +1,4 @@
-DOMAIN = 'YOUR_COMPANY_DOMAIN_NAME'
-API_KEY = 'YOUR_PERSONAL_API_KEY'
+DOMAIN = 'YOUR_COMPANY_DOMAIN_NAME'  # Domain you uses to login via BambooHR auth form
+API_KEY = 'YOUR_PERSONAL_API_KEY'  # Generated API_KEY
+API_REQUEST_GATE = ':x@api.bamboohr.com/api/gateway.php/'
+API_DIRECTORY_REQUEST = '/v1/employees/directory'
