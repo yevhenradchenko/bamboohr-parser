bf1f2d11e618e546f033aa3c3a95541c69413f89
Yevhen Radchenko <yevhen.y.radchenko@gmail.com>
2018-09-26 13:23:05 +0300

    Test fixes after refactoring

--- tests/tests.py
+++ tests/tests.py
@@ -1,27 +1,33 @@
 import unittest
-import bamboohr_to_database as master
+from bamboohr_to_database import *
 
 
-class TestRequests(unittest.TestCase):
+class TestRequests(unittest.TestCase, BambooParser):
 
     def test_database_init(self):
-        db_init = master.database_init()
-        self.assertEqual(db_init, )
+        parser = BambooParser(url=BAMBOO_URL)
+        db_init = parser._database_init()
+        self.assertEqual(db_init, None)
 
     def test_get_request(self):
-        response = master.bamboo_request(url=master.BAMBOO_URL)
+        parser = BambooParser(url=BAMBOO_URL)
+        response = parser._get_employees(url=BAMBOO_URL)
         self.assertNotEqual(response, False, msg='HTTP Request is FAILED')
 
     def test_available_fields(self):
-        fields = master.available_fields()
+        parser = BambooParser(url=BAMBOO_URL)
+        fields = parser.available_fields()
         self.assertEqual(type(fields), type(''), msg='Some fields may be changed -> check XML file')
 
     def test_parser(self):
-        parser = master.bamboo_parse()
-        self.assertEqual(parser, None, msg='Cannot parse some fields -> check XML file')
+        parser = BambooParser(url=BAMBOO_URL)
+        parsed = parser.bamboo_parse()
+        self.assertEqual(parsed, None, msg='Cannot parse some fields -> check XML file')
 
     def test_writing(self):
-        session = master.write_session()
+        parser = BambooParser(url=BAMBOO_URL)
+        emp = parser.bamboo_parse()
+        session = parser._write_session(emp)
         self.assertEqual(session, None, msg='Something wrong with file writing')
 
 
